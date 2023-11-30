58c3093302797fedb330f894e9b1e0fb14e343cd
Klantendienst | IT Spot Putte <putte@itspot.be>
2023-11-30 11:51:50 +0100

    Initial commit

---
+++ include/cta-nieuwsbrief.php
@@ -0,0 +1,18 @@
+
+<div class="py-5 bg-primary block-4">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <h3 class="text-white">Abonneer op Nieuwsbrief</h3>
+            <p class="opacity-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, reprehenderit!</p>
+          </div>
+          <div class="col-lg-6">
+            <form action="#" class="form-subscribe d-flex align-items-stretch">
+              <input type="email" class="form-control h-100" placeholder="Voer uw e-mail in">
+              <input type="submit" class="btn btn-secondary px-4" value="Abonneer">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
---
+++ include/header.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Untree.co">
+    <link rel="shortcut icon" href="favicon.png">
+
+    <meta name="description" content=""/>
+    <meta name="keywords" content="bootstrap, bootstrap4"/>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Open+Sans&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+    <link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Agriculture Free Bootstrap Template by Untree.co</title>
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+<div class="site-wrap">
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <div class="header-top bg-light">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6 col-lg-3">
+                    <a href="index.html" class="logo">
+                        <img src="../images/part1/decuyperwitters.png" width="60" alt="Image" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col-lg-3 d-none d-lg-block">
+                    <div class="quick-contact-icons d-flex align-items-center">
+                        <div class="icon align-self-center mt-3">
+                            <span class="icon-room text-primary"></span>
+                        </div>
+                        <div class="text">
+                            <span class="h4 d-block">Koningsbaan 40 - 2220 Heist-op-den-Berg</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 d-none d-lg-block">
+                    <div class="quick-contact-icons d-flex align-items-center">
+                        <div class="icon align-self-center mt-3">
+                            <span class="icon-phone text-primary"></span>
+                        </div>
+                        <div class="text">
+                            <span class="h4 d-block"><a href="tel:015233237" target="_blank">01523.32.37</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 d-none d-lg-block">
+                    <div class="quick-contact-icons d-flex align-items-center">
+                        <div class="icon align-self-center mt-3">
+                            <span class="icon-envelope text-primary"></span>
+                        </div>
+                        <div class="text">
+                            <span class="h4 d-block"><a href="mailto:decuyper-gilbert@hotmail.com" target="_blank">decuyper-gilbert@hotmail.com</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-block d-lg-none text-right">
+                    <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                                class="icon-menu h3"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
+
+    <div class="container">
+        <div class="d-flex align-items-center">
+
+            <div class="mx-auto">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+                    <ul class="site-menu main-menu js-clone-nav mr-auto d-none pl-0 d-lg-block">
+                        <li class="active">
+                            <a href="index.html" class="nav-link text-left">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html" class="nav-link text-left">Over ons</a>
+                        </li>
+                        <li>
+                            <a href="services.html" class="nav-link text-left">diensten</a>
+                        </li>
+                        <li>
+                            <a href="testimonials.html" class="nav-link text-left">Testimonials</a>
+                        </li>
+                        <li><a href="blog.html" class="nav-link text-left">Blog</a></li>
+                        <li>
+                            <a href="contact.html" class="nav-link text-left">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+
